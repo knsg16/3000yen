629bf3085d76b5ec5fd5a6a34aea8cf63ad3b9da
Yuki Kanasugi <yuki.kanasugi@okan.jp>
2019-04-28 18:00:26 +0900

    Componentの作成方法

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// ./は自分のいる階層を表す
+import BodyText from './src/elements/BodyText';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello</Text>
+        <BodyText>Hi</BodyText>
       </View>
     );
   }
