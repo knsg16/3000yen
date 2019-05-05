977e18dd310ab630ad4fc5ffb20222f4cb5fad62
Yuki Kanasugi <yuki.kanasugi@okan.jp>
2019-05-05 10:48:20 +0900

    Add header to todayListScreen

--- src/screens/TodayListScreen.js
+++ src/screens/TodayListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import firebase from 'firebase';
 import moment from 'moment';
 
@@ -16,6 +16,7 @@ class MemoListScreen extends React.Component {
 
   // componentが表示される前に処理が行われるということ
   componentWillMount() {
+    console.log('props: ', this.props.amount);
     const { currentUser } = firebase.auth();
     const db = firebase.firestore();
     db.collection(`users/${currentUser.uid}/records`)
@@ -38,6 +39,19 @@ class MemoListScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={[styles.box, { backgroundColor: '#ddd' }]}>
+          <View style={styles.headerBox}>
+            <Text style={styles.header}>今日残り金額</Text>
+          </View>
+          <View style={styles.contentBox}>
+            <Text style={styles.date}>{today.format('MM/DD')}</Text>
+            <Text style={styles.amount}>
+              {this.props.amount}
+              円
+            </Text>
+          </View>
+        </View>
+
         <MemoList memoList={this.state.memoList} navigation={this.props.navigation} />
         <CircleButton name="plus" onPress={this.handlePress.bind(this)} />
       </View>
@@ -50,6 +64,47 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     backgroundColor: '#FFFDF6',
+  },
+  box: {
+    padding: 10,
+    width: '90%',
+    height: 96,
+    // borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+  },
+  headerBox: {
+    width: '40%',
+    // borderWidth: 1,
+    fontSize: 24,
+    color: '#fff',
+  },
+  contentBox: {
+    width: '60%',
+    // borderWidth: 1,
+    fontSize: 24,
+    // color: '#fff',
+  },
+  header: {
+    fontSize: 24,
+    // color: '#fff',
+  },
+  date: {
+    // borderWidth: 1,
+    // color: '#fff',
+    height: '30%',
+  },
+  amount: {
+    // borderWidth: 1,
+    height: '70%',
+    fontSize: 32,
+    textAlign: 'right',
+    // color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
